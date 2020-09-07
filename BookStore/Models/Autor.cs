4dd5b20584f84eca59a795e4e752173a5fc790ff
Lucian AF <lucian_af@hotmail.com>
2020-09-07 13:10:31 -0300

    Criação do projeto BookStore; Modelando o domínio;

---
+++ BookStore/Models/Autor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BookStore.Models
+{
+    public class Autor
+    {
+        public Autor()
+        {
+            this.Livros = new List<Livro>();
+        }
+
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public ICollection<Livro> Livros { get; set; }
+    }
+}
---
+++ BookStore/Models/Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookStore.Models
+{
+    public class Livro
+    {
+        public Livro()
+        {
+            this.Autores = new List<Autor>();
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string ISBN { get; set; }
+        public DateTime DataLancamento { get; set; }
+
+        public int CategoriaId { get; set; }
+        /// <summary>
+        /// Usar o virtual antes do tipo do atributo, faz com que ele traga a Categoria sobre demanda, 
+        /// ou seja, somente se você precisar da Categoria na sua consulta ele trará a Categoria;
+        /// </summary>
+        public Categoria Categoria { get; set; }
+
+        public ICollection<Autor> Autores { get; set; }
+    }
+}
