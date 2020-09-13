a23821a0cadbc66db8dc230d7c09c5cba706f722
Lucian AF <lucian_af@hotmail.com>
2020-09-13 20:50:26 -0300

    Adicionado Projeto demo Autenticacao; Abordagem do Identity e seu funcionamento;

---
+++ Autenticacao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Autenticacao.Startup))]
+namespace Autenticacao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
