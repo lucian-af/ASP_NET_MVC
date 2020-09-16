32367d201e4c175fc329d1abbc40ef6eb5bea7c8
Lucian AF <lucian_af@hotmail.com>
2020-09-15 23:48:57 -0300

    Repaginando o Layout padrão; Somente Autor foi atualizado.

--- BookStore/App_Start/BundleConfig.cs
+++ BookStore/App_Start/BundleConfig.cs
@@ -27,7 +27,8 @@ namespace BookStore
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/style.css"));
 
             bundles.Add(new StyleBundle("~/Content/custom").Include(
                       "~/Content/style.css"));
--- BookStore/Scripts/Sistema/script.js
+++ BookStore/Scripts/Sistema/script.js
@@ -1,4 +1,6 @@
 ﻿$(document).ready(() => {
     // seta foco no primeiro elemento input que não esteja disabled, hidden ou readonly
     $("input:not(:disabled):not(input[type='hidden']):not([readonly])").first().focus();
+
+    $(".padrao").css('height', '' + ($("footer").offset().top - 105) + 'px');
 });
